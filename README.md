864a50c9212dbe5f1a54253e948de7ae7814e253
龙  张 <longzhang@longdeMacBook-Air.local>
2017-07-21 11:02:18 +0800

    图片放大

--- README.md
+++ README.md
@@ -1,10 +1,40 @@
-# vue-
-Vue业务插件
+# enlargeImage
+
+vue 图片放大插件
+
+```
+  npm i enlarge-img --save
+
+```
+
+ 
+  如果你安装了淘宝镜像 可以使用cnpm i enlarge-img --save <br/>
 
 
-[Vue dialog原生样式的插件](https://github.com/zhanglongdream/vue-plun/tree/dialog-pc)<br/>
-[vue翻页插件](https://github.com/zhanglongdream/vue-plun/tree/paginate)<br/>
-[Vue ios select原生样式的插件](https://github.com/zhanglongdream/vue-plun/tree/select-ios)<br/>
-[Vue spinner 等待样式](https://github.com/zhanglongdream/vue-plun/tree/spinner)<br/>
-[Vue toast 提示信息](https://github.com/zhanglongdream/vue-plun/tree/toast)<br/>
-[Vue 操作延长插件](https://github.com/zhanglongdream/vue-plun/tree/vue-message)<br/>
+  如果没有安装建议安装一个毕竟国内的影响心情 [点击安装](https://npm.taobao.org/)
+  
+
+```
+使用方法  
+ <bigimg :zlImgSrc="imgSrc" :showhidden ="shows" v-on:imghidden="bigImgs"></bigimg>
+
+ 引入组件
+
+ :zlImgSrc 是放大图片的地址，，即你想要放大图片的地址
+ :showhidden 控制图片的显示隐藏  默认是false
+ v-on:imghidden 点击图片或者整个窗口的时候放大后的图片消失
+```
+
+
+
+```
+
+```
+
+
+
+
+ 如果有什么bug欢迎指出  [欢迎指正](https://github.com/zhanglongdream/vue-plun/issues/7)
+
+  
+   最后帮忙来个start呗，thanks ☺☺;
---
+++ enlargeImage.vue
@@ -0,0 +1,82 @@
+<template>
+  <transition name="zl-fade" v-if="showhidden">
+    <div class="zl-img-view" @click="hiddenImg">
+      <div class="zl-img-layer"></div>
+      <img :src="zlImgSrc"/>
+    </div>
+  </transition>
+</template>
+<script>
+  export default {
+    props:{
+      zlImgSrc:{
+        type:String,
+        default:''
+      },
+      showhidden:{
+        type:Boolean,
+        default:false
+      }
+    },
+    methods:{
+      hiddenImg(){
+        this.$emit('imghidden' , false)
+      }
+    }
+  }
+</script>
+<style lang="css" scoped>
+  .zl-fade-enter-active{    
+    animation:zl-fade-enterleave .3s ease;
+  }
+  
+  .zl-fade-leave-active{    
+    animation:zl-fade-leave .2s ease forwards;
+  }
+  @keyframes zl-fade-enterleave {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      
+    }
+  }
+
+  @keyframes zl-fade-leave {
+    0% {
+      
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+
+  .zl-img-view .zl-img-layer{
+    position: absolute;
+    left:0;
+    top:0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background: rgba(0,0,0,.7);
+    z-index: 9998;
+  }
+  .zl-img-view{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
+  .zl-img-view img{
+    display: block;
+    max-width: 100%;
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: auto;
+    z-index: 9999;
+    top: 0;
+    bottom: 0;
+  }
+</style>
